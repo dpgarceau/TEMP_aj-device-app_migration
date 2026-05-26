e83b0f8cb2b6d22b94eba077fbfb6fcd6b65b5e0
dpgarceau <david@lykicare.com>
2026-05-26 16:13:25 -0400

    Make judge keypad edge triggered

--- judge/src/main/resources/static/device-keys.js
+++ judge/src/main/resources/static/device-keys.js
@@ -13,22 +13,28 @@
         '8': 'plus-half',
         '9': 'plus-one'
     };
+    var pressedDigits = {};
+
+    function eventSource(event) {
+        return event && event.originalEvent ? event.originalEvent : event;
+    }
 
     function digitFromEvent(event) {
         if (!event) {
             return null;
         }
+        var source = eventSource(event);
 
-        if (event.code && event.code.indexOf('Numpad') === 0) {
-            return event.code.substring('Numpad'.length);
+        if (source.code && source.code.indexOf('Numpad') === 0) {
+            return source.code.substring('Numpad'.length);
         }
 
-        if (event.code && event.code.indexOf('Digit') === 0) {
-            return event.code.substring('Digit'.length);
+        if (source.code && source.code.indexOf('Digit') === 0) {
+            return source.code.substring('Digit'.length);
         }
 
-        if (/^[0-9]$/.test(event.key)) {
-            return event.key;
+        if (/^[0-9]$/.test(source.key)) {
+            return source.key;
         }
 
         if (event.which >= 48 && event.which <= 57) {
@@ -42,14 +48,31 @@
         return null;
     }
 
+    function releaseFromEvent(event) {
+        var digit = digitFromEvent(event);
+        if (digit) {
+            delete pressedDigits[digit];
+        }
+    }
+
+    window.addEventListener('keyup', releaseFromEvent, true);
+    window.addEventListener('blur', function () {
+        pressedDigits = {};
+    });
+
     window.AeroJudgeDeviceKeys = {
         actionFromEvent: function (event) {
+            var digit = digitFromEvent(event);
+            var source = eventSource(event);
+            var action = digitActions[digit];
+
             // Judge buttons are edge-triggered: holding a GPIO key must not repeat score/navigation actions.
-            if (event && event.repeat) {
+            if (!action || pressedDigits[digit] || (source && source.repeat)) {
                 return null;
             }
 
-            return digitActions[digitFromEvent(event)] || null;
+            pressedDigits[digit] = true;
+            return action;
         }
     };
 })(window);
